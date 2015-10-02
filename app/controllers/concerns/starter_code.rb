21df0ced0ed23384a0c90ad5661cd537fc5b8c39
Mark Tareshawty <mark@marktareshawty.com>
2015-10-02 15:06:38 -0400

    catch ArgumentError from GitHub, and handle it

--- app/controllers/concerns/starter_code.rb
+++ app/controllers/concerns/starter_code.rb
@@ -1,8 +1,15 @@
 module StarterCode
+  include GitHub
+  extend ActiveSupport::Concern
+
   def starter_code_repository_id(repo_name)
     return unless repo_name.present?
-    sanitized_repo_name = repo_name.gsub(/\s+/, '')
-    github_repository   = GitHubRepository.new(current_user.github_client, nil)
-    github_repository.repository(sanitized_repo_name).id
+
+    begin
+      github_repository = GitHubRepository.new(current_user.github_client, nil)
+      github_repository.repository(repo_name).id
+    rescue ArgumentError => err
+      raise GitHub::Error, err.message
+    end
   end
 end
